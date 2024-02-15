3d17108694eeaca7bfb7d70f8b559763d32b7d9c
SergeyMMedvedev <medvedevsm1989@yandex.ru>
2024-02-15 11:49:07 +0300

    init

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+
+	var buf string
+	var esc bool
+	res := strings.Builder{}
+	for _, b := range str {
+		letter := fmt.Sprintf("%c", b)
+		digit, err := strconv.Atoi(letter)
+		if letter == `\` && !esc {
+			esc = true
+		} else if err != nil && letter != `\` && esc {
+			return "", ErrInvalidString
+		} else if err != nil || esc {
+			res.Write([]byte(buf))
+			buf = letter
+			if esc {
+				esc = false
+			}
+		} else {
+			if buf == "" {
+				return "", ErrInvalidString
+			} else {
+				res.Write([]byte(strings.Repeat(buf, digit)))
+				buf = ""
+			}
+		}
+	}
+	if esc {
+		return "", ErrInvalidString
+	}
+	res.Write([]byte(buf))
+	return res.String(), nil
 }
