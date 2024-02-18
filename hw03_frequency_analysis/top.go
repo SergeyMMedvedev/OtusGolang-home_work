efa230e7f80abfcbe67246f5717835d8dc567011
Sergey Medvedev <medvedevsm1989@yandex.ru>
2024-02-18 22:59:35 +0300

    hw init

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,33 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var wordRegex = regexp.MustCompile(
+	`([а-яА-Я\w]+(?:[-\.,]+[-а-яА-Я\w]+)?)|-{2,}`,
+)
+
+func Top10(str string) (res []string) {
+	fields := wordRegex.FindAllString(strings.ToLower(str), -1)
+	freq := make(map[string]int)
+	arr := make([][]string, len(fields))
+	for _, word := range fields {
+		freq[word]++
+	}
+	for k, v := range freq {
+		arr[v] = append(arr[v], k)
+	}
+	for i := len(arr) - 1; i >= 0; i-- {
+		if len(arr[i]) > 0 {
+			sort.Strings(arr[i])
+			res = append(res, arr[i]...)
+		}
+	}
+	if len(res) > 10 {
+		return res[:10]
+	}
+	return res
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -63,6 +63,28 @@ func TestTop10(t *testing.T) {
 				"не",        // 4
 			}
 			require.Equal(t, expected, Top10(text))
+			expected = []string{
+				"and",     // 2
+				"one",     // 2
+				"cat",     // 1
+				"cats",    // 1
+				"dog",     // 1
+				"dog,two", // 1
+				"man",     // 1
+			}
+			require.Equal(t, expected, Top10(
+				"cat and dog, one dog,two cats and one man",
+			))
+			expected = []string{
+				"нога",     // 5
+				"---",      // 2
+				"--",       // 1
+				"какой-то", // 1
+				"какойто",  // 1
+			}
+			require.Equal(t, expected, Top10(
+				"Нога нога! нога нога, 'нога' какой-то какойто --- --- - - - --",
+			))
 		} else {
 			expected := []string{
 				"он",        // 8
