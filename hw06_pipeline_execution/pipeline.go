656dc96e82b9c35705acc7d34e6b4acf6727e325
Sergey Medvedev <medvedevsm1989@yandex.ru>
2024-03-18 21:34:09 +0300

    init

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,9 @@
 package hw06pipelineexecution
 
+import (
+	"fmt"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -8,7 +12,44 @@ type (
 
 type Stage func(in In) (out Out)
 
+func (s Stage) String() string {
+	return fmt.Sprintf("%T", s)
+}
+
+func ExecuteStage(in In, done In, stage Stage) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for v := range stage(in) {
+			select {
+			case <-done:
+				return
+			case out <- v:
+			}
+		}
+	}()
+	return out
+}
+
+// func ExecutePipeline(in In, done In, stages ...Stage) Out {
+// 	// Place your code here.
+// 	switch len(stages) {
+// 	case 0:
+// 		return in
+// 	case 1:
+// 		return ExecuteStage(in, done, stages[0])
+// 	default:
+// 		return ExecutePipeline(ExecuteStage(in, done, stages[0]), done, stages[1:]...)
+// 	}
+// }
+
+//	func ExecutePipeline(in In, done In, stages ...Stage) Out {
+//		// Place your code here.
+//		for counter := 0; counter < len(stages); counter++ {
+//			in = ExecuteStage(in, done, stages[counter])
+//		}
+//		return in
+//	}
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	return stages[3](stages[2](stages[1](stages[0](in))))
 }
