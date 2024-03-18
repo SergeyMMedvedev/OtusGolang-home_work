4d278796c54928de4cd1369420fecc2d1c58fc8b
Sergey Medvedev <medvedevsm1989@yandex.ru>
2024-03-18 22:35:06 +0300

    iniy

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -31,25 +31,10 @@ func ExecuteStage(in In, done In, stage Stage) Out {
 	return out
 }
 
-// func ExecutePipeline(in In, done In, stages ...Stage) Out {
-// 	// Place your code here.
-// 	switch len(stages) {
-// 	case 0:
-// 		return in
-// 	case 1:
-// 		return ExecuteStage(in, done, stages[0])
-// 	default:
-// 		return ExecutePipeline(ExecuteStage(in, done, stages[0]), done, stages[1:]...)
-// 	}
-// }
-
-//	func ExecutePipeline(in In, done In, stages ...Stage) Out {
-//		// Place your code here.
-//		for counter := 0; counter < len(stages); counter++ {
-//			in = ExecuteStage(in, done, stages[counter])
-//		}
-//		return in
-//	}
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	return stages[3](stages[2](stages[1](stages[0](in))))
+	// Place your code here.
+	for counter := 0; counter < len(stages); counter++ {
+		in = ExecuteStage(in, done, stages[counter])
+	}
+	return in
 }
