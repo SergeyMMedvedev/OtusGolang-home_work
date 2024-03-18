0e8abdfb31431e6b5a33badd37371176ce5832be
Sergey Medvedev <medvedevsm1989@yandex.ru>
2024-03-18 22:39:28 +0300

    init2

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -12,10 +12,6 @@ type (
 
 type Stage func(in In) (out Out)
 
-func (s Stage) String() string {
-	return fmt.Sprintf("%T", s)
-}
-
 func ExecuteStage(in In, done In, stage Stage) Out {
 	out := make(Bi)
 	go func() {
