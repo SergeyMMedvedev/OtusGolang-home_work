49afb7103ec50d2167c7486e11b989ffd681ae7d
Sergey Medvedev <medvedevsm1989@yandex.ru>
2024-04-21 17:45:02 +0300

    init

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,75 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	testDir := "./testdata/env"
+	expected := Environment{
+		"BAR": EnvValue{
+			"bar",
+			false,
+		},
+		"EMPTY": EnvValue{
+			"",
+			false,
+		},
+		"FOO": EnvValue{
+			"   foo\nwith new line",
+			false,
+		},
+		"HELLO": EnvValue{
+			"\"hello\"",
+			false,
+		},
+	}
+	env, err := ReadDir(testDir)
+	require.NoError(t, err)
+	require.Equal(t, expected, env)
+	_, ok := os.LookupEnv("UNSET")
+	require.False(t, ok)
+}
+
+func TestReadDirReplace(t *testing.T) {
+	testDir := "./testdata/env"
+	expected := Environment{
+		"BAR": EnvValue{
+			"bar",
+			true,
+		},
+		"EMPTY": EnvValue{
+			"",
+			false,
+		},
+		"FOO": EnvValue{
+			"   foo\nwith new line",
+			false,
+		},
+		"HELLO": EnvValue{
+			"\"hello\"",
+			true,
+		},
+	}
+	os.Setenv("BAR", "bar")
+	os.Setenv("HELLO", "hello")
+	env, err := ReadDir(testDir)
+	require.NoError(t, err)
+	require.Equal(t, expected, env)
+}
+
+func TestReadDirIgnoreEquals(t *testing.T) {
+	testDir := "./testdata/env2"
+	expected := Environment{
+		"FOO": EnvValue{
+			"   foo\nwith new line",
+			false,
+		},
+	}
+	env, err := ReadDir(testDir)
+	require.NoError(t, err)
+	require.Equal(t, expected, env)
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	command.Env = os.Environ()
+	for k, envValue := range env {
+		if envValue.NeedRemove {
+			os.Unsetenv(k)
+		}
+		command.Env = append(command.Env, fmt.Sprintf("%s=%s", k, envValue.Value))
+	}
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	err := command.Start()
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	err = command.Wait()
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	return 0
 }
