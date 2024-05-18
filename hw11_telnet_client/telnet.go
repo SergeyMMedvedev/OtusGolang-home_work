c5e7e4f5885b334758fa8b25fd19a56ec319cd16
Sergey Medvedev <medvedevsm1989@yandex.ru>
2024-05-18 21:34:29 +0300

    123

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -24,32 +24,21 @@ type telnetClient struct {
 	in      io.ReadCloser
 	out     io.Writer
 
-	inChan  chan string
-	outChan chan string
-	conn    net.Conn
-
+	conn       net.Conn
 	outScanner bufio.Scanner
 }
 
 func (c *telnetClient) Send() error {
+	scanner := bufio.NewScanner(c.in)
 	go func() {
-		for str := range c.inChan {
-			c.conn.Write([]byte(fmt.Sprintf("%s\n", str)))
-		}
-	}()
-	return nil
-}
-
-func (c *telnetClient) inScan() {
-	go func() {
-		scanner := bufio.NewScanner(c.in)
 		for scanner.Scan() {
-			c.inChan <- scanner.Text()
+			c.conn.Write([]byte(fmt.Sprintf("%s\n", scanner.Text())))
 		}
 		if scanner.Err() != nil {
-			close(c.inChan)
+			return
 		}
 	}()
+	return scanner.Err()
 }
 
 func (c *telnetClient) Connect() error {
@@ -65,7 +54,6 @@ func (c *telnetClient) Connect() error {
 	}
 	log.Printf("connect from %s to %s\n", c.conn.LocalAddr(), c.conn.RemoteAddr())
 	c.outScanner = *bufio.NewScanner(c.conn)
-	c.inScan()
 
 	return nil
 }
@@ -80,7 +68,6 @@ func (c *telnetClient) Close() error {
 	if err != nil {
 		return fmt.Errorf("close input: %w", err)
 	}
-	close(c.inChan)
 	return nil
 }
 
@@ -95,16 +82,11 @@ func (c *telnetClient) Receive() error {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	inChan := make(chan string)
-	outChan := make(chan string)
 	return &telnetClient{
 		Address: address,
 		timeout: timeout,
 		in:      in,
 		out:     out,
-
-		inChan:  inChan,
-		outChan: outChan,
 	}
 }
 
